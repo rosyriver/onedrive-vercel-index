effb49e65f4b2519e519c4f41665326e0c3f7a62
postman1year <postman1year@gmail.com>
2025-02-09 01:57:58 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   i18n,
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
