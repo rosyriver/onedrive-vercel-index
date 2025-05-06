29e1ef3dbda52881ee8ab313f31a86c90ee22787
postman1year <postman1year@gmail.com>
2025-05-06 22:50:25 +0800

    Add files via upload

--- config/config.ts
+++ config/config.ts
@@ -1,12 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import apiConfig from './api.config'
-import siteConfig from './site.config'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
-    clientId: apiConfig.clientId,
-    clientSecret: apiConfig.obfuscatedClientSecret,
-    userPrincipalName: siteConfig.userPrincipalName,
-    baseDirectory: siteConfig.baseDirectory
+    clientId: process.env.CLIENT_ID || '',
+    clientSecret: process.env.CLIENT_SECRET || '',
+    userPrincipalName: process.env.USER_PRINCIPAL_NAME || '',
+    baseDirectory: process.env.BASE_DIRECTORY || '/'
   })
 }
