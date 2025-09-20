fa150508bcc26aa57326bbc829ca2e43241a5ea9
postman1year <postman1year@gmail.com>
2025-09-20 22:45:46 +0800

    2025年9月20日最新版本
        攻克難題, 將React, React-dom, React-Markdown由原來的V8升級至最新版本V10   (2025-6-1)
        於2025年6月1日完成所有元件升級最新版本並VERCEL編譯成功的版本,
    
        * !!!但編譯成功後, 載入網頁時, 竟然出現Application error a client-side exception has occurred (see the browser console for more information), 無法載入網頁!!! 
    
        於是回復到5月30日版本則一切沒有問題
    
        於2025年6月2日逐個元件升級上傳測試所得, 踩坑很多, 事實上React由18升級至19我是一直修改成功的,
    
        導致出現client-side exception has occurred無法載入網頁的唯一原因是React-Cookie (src\components\SwitchLang.tsx), 
        ***只要升級React-Cookie至8.0.1則必然出現無法載入網頁 的嚴重錯誤!!!!  ***
    
        故最終版本移除React-Cookie, 換成JS-Cookie
        
        現在, 所有元件均升級至最新版本, 當然Node是22x LTS   ( VERCEL不能 升級至23或24新的非LTS版本)

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -4,10 +4,10 @@ import { Menu, Transition } from '@headlessui/react'
 
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { useCookies, withCookies } from 'react-cookie'
+import Cookies from 'js-cookie'
 
 // https://headlessui.dev/react/menu#integrating-with-next-js
-const CustomLink = ({ href, children, as, locale, ...props }): JSX.Element => {
+const CustomLink = ({ href, children, as, locale, ...props }): React.ReactNode => {
   return (
     <Link href={href} as={as} locale={locale} {...props}>
       {children}
@@ -30,8 +30,6 @@ const localeText = (locale: string): string => {
 
 const SwitchLang = () => {
   const { locales, pathname, query, asPath } = useRouter()
-
-  const [_, setCookie] = useCookies(['NEXT_LOCALE'])
 
   return (
     <div className="relative">
@@ -58,7 +56,7 @@ const SwitchLang = () => {
                   href={{ pathname, query }}
                   as={asPath}
                   locale={locale}
-                  onClick={() => setCookie('NEXT_LOCALE', locale, { path: '/' })}
+                  onClick={() => Cookies.set('NEXT_LOCALE', locale, { path: '/' })}
                 >
                   <div className="m-1 cursor-pointer rounded-sm px-2 py-1 text-left text-sm font-medium hover:bg-blue-50 hover:text-blue-700 dark:hover:bg-blue-600/10 dark:hover:text-blue-400">
                     {localeText(locale)}
@@ -73,4 +71,4 @@ const SwitchLang = () => {
   )
 }
 
-export default withCookies(SwitchLang)
+export default SwitchLang
